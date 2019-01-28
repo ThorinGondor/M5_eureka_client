b394b69b7b664a8eff3d8420447678d013b85d09
ThorinGondor <1360002051@qq.com>
2019-01-28 17:37:55 +0800

    扩展Client服务接口

--- src/main/java/com/hystrix_demo/eureka_client/controller/HelloServiceController.java
+++ src/main/java/com/hystrix_demo/eureka_client/controller/HelloServiceController.java
@@ -1,10 +1,9 @@
 package com.hystrix_demo.eureka_client.controller;
 
+import com.hystrix_demo.eureka_client.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloServiceController {
@@ -15,5 +14,23 @@ public class HelloServiceController {
     public String hello(){
         discoveryClient.getServices();
         return "Hello Eureka Client!";
+    }
+
+    /**
+     * Modify: 以下三个方法为Feign消费者提供更多的服务，Feign消费者将会调用这些服务
+     */
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    public String hello(@RequestParam String name){
+        return "Hello! "+name+"\n";
+    }
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public User hello(@RequestHeader String name, @RequestHeader Integer age){
+        return new User(name, age);
+    }
+
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    public String hello(@RequestBody User user){
+        return "\nHello "+user.getName()+", "+user.getAge();
     }
 }
